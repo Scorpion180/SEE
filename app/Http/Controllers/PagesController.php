65895e19647cde9b6e5e9a532484f7c19024bcbd
Efren <efren.hernandez@alumnos.udg.mx>
2019-02-21 16:40:41 -0600

    Agrega PagesController

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function info(){
+        return view("pages.informacion");
+    }
+
+    public function contacto(){
+        return view("pages.contacto");
+    }
+    public function bienvenido($nombre,$apellido = null){
+        return view('pages.bienvenida',compact('nombre','apellido'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,24 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/info', function () {
-    return view("pages.informacion");
-});
+Route::get('/info','PagesController@info');
 
-Route::get('/contacto', function () {
-    return view("pages.contacto");
-});
+Route::get('/contacto', 'PagesController@contacto');
 
 //apellido? para indicar que puede o no recibirlo
-Route::get('/bienvenida/{nombre}/{apellido?}',function($nombre,$apellido = null){
-    return view('pages.bienvenida',compact('nombre','apellido'));
-    
-    //Para mandar valores a la vista
+Route::get('/bienvenida/{nombre}/{apellido?}','PagesController@bienvenido');
+//Para mandar valores a la vista
     //->with(['nombre'=>$nombre,'apellido'=>$apellido]);
 
     //Compact para los mismos nombres de variables que recibiste
     //return view('pages.bienvenida',compact('nombre','apellido'));
-});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
