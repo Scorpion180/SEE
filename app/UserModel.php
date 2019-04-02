ffb573ce08d92001e9092519f3ce5a57eb0939f5
Efren <efren.hernandez@alumnos.udg.mx>
2019-04-02 16:38:05 -0600

    Agrega seeder y factory de departamento

--- app/UserModel.php
+++ app/UserModel.php
@@ -8,7 +8,7 @@ class UserModel extends Model
 {
     protected $table = 'users';
     protected $fillable = ['name','email','codigo','password'];
-    public function profesores(){
-        return $this->hasMany('profesores');
-    }
+    //public function profesores(){
+      //  return $this->hasMany('profesores');
+    //}
 }
